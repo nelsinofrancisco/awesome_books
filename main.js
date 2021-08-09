4c46c65e4287d2aa629797319844c3e361583ea9
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-08-10 00:25:36 +0200

    Add generate function

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const BOOKS_DATA = [
   { title: 'second book', author: 'person 2' },
 ];
 
-const form = document.querySelector('form');
-const removeBtns = document.querySelectorAll('.remove-book');
+let form = document.querySelector('form');
+let removeBtns = document.querySelectorAll('.remove-book');
+let bookList = document.getElementById('book-list');
+
+function updateSelections() {
+  
+  form = document.querySelector('form');
+  removeBtns = document.querySelectorAll('.remove-book');
+  bookList = document.getElementById('book-list');
+
+};
 
 function addBook() {
   const bookTitle = document.getElementById('book-title');
@@ -22,13 +31,60 @@ function removeBook(index) {
   BOOKS_DATA.splice(index, 1);
 }
 
+function generateBookCard(bookObj) {
+  const bookCard = document.createElement('li');
+  const bookTitle = document.createElement('p');
+  const bookAuthor = document.createElement('p');
+  const removeBtn = document.createElement('button');
+
+  bookCard.classList.add('book-card', 'mb-1');
+  bookTitle.classList.add('book-card-text');
+  bookAuthor.classList.add('book-card-text');
+  removeBtn.classList.add('remove-book', 'mb-05');
+
+  bookTitle.textContent = bookObj.title;
+  bookAuthor.textContent = bookObj.author;
+  removeBtn.textContent = 'Remove';
+  removeBtn.onclick = assignRemoveEvents();
+
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(removeBtn);
+  return bookCard;
+}
+
+function clearBooksSection () {
+  bookList.innerHTML = '';
+}
+
+function displayBooksSection(bookArray) {
+
+  for (let i = 0; i < bookArray.length; i += 1){
+    const bookCard = generateBookCard(bookArray[i]);
+
+    bookList.appendChild(bookCard);
+  }
+}
+
+displayBooksSection(BOOKS_DATA);
+updateSelections();
+
 form.addEventListener('submit', (event) => {
   BOOKS_DATA.push(addBook());
+  clearBooksSection();
+  displayBooksSection(BOOKS_DATA);
+  updateSelections();
   event.preventDefault();
 });
 
-for (let i = 0; i < removeBtns.length; i += 1) {
-  removeBtns[i].addEventListener('click', () => {
-    removeBook(i);
-  });
+function assignRemoveEvents() {
+  const removeBtns = document.querySelectorAll('.remove-book');
+  for (let i = 0; i < removeBtns.length; i += 1) {
+    removeBtns[i].addEventListener('click', () => {
+      removeBook(i);
+      clearBooksSection();
+      displayBooksSection(BOOKS_DATA);
+      updateSelections();
+    });
+  }
 }
