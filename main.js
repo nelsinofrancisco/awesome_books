4ae318f5da4c607dee8d8c0bcbcbd18d2043bdde
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-08-09 22:24:07 -0300

    Add localstorage data management

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-const BOOKS_DATA = [
-  { title: 'first book', author: 'person 1' },
-  { title: 'second book', author: 'person 2' },
-];
+let BOOKS_DATA = [];
+
+function setStorage() {
+  localStorage.setItem('books_data', JSON.stringify(BOOKS_DATA));
+}
+
+function retrieveStorage() {
+  BOOKS_DATA = JSON.parse(localStorage.getItem('books_data'));
+}
+
+if (!localStorage.getItem('books_data')) {
+  setStorage();
+} else {
+  retrieveStorage();
+}
 
 let form = document.querySelector('form');
-let removeBtns = document.querySelectorAll('.remove-book');
 let bookList = document.getElementById('book-list');
 
 function updateSelections() {
-  
   form = document.querySelector('form');
-  removeBtns = document.querySelectorAll('.remove-book');
   bookList = document.getElementById('book-list');
-
-};
+}
 
 function addBook() {
   const bookTitle = document.getElementById('book-title');
@@ -27,8 +34,16 @@ function addBook() {
   return bookObj;
 }
 
-function removeBook(index) {
-  BOOKS_DATA.splice(index, 1);
+function removeBook(element) {
+  const textElements = element.parentElement.querySelectorAll('.book-card-text');
+
+  BOOKS_DATA = BOOKS_DATA.filter((obj) => {
+    if (textElements[0].innerText === obj.title && textElements[1].innerText === obj.author) {
+      return false;
+    }
+
+    return true;
+  });
 }
 
 function generateBookCard(bookObj) {
@@ -45,7 +60,6 @@ function generateBookCard(bookObj) {
   bookTitle.textContent = bookObj.title;
   bookAuthor.textContent = bookObj.author;
   removeBtn.textContent = 'Remove';
-  removeBtn.onclick = assignRemoveEvents();
 
   bookCard.appendChild(bookTitle);
   bookCard.appendChild(bookAuthor);
@@ -53,15 +67,13 @@ function generateBookCard(bookObj) {
   return bookCard;
 }
 
-function clearBooksSection () {
+function clearBooksSection() {
   bookList.innerHTML = '';
 }
 
 function displayBooksSection(bookArray) {
-
-  for (let i = 0; i < bookArray.length; i += 1){
+  for (let i = 0; i < bookArray.length; i += 1) {
     const bookCard = generateBookCard(bookArray[i]);
-
     bookList.appendChild(bookCard);
   }
 }
@@ -69,22 +81,21 @@ function displayBooksSection(bookArray) {
 displayBooksSection(BOOKS_DATA);
 updateSelections();
 
+document.addEventListener('click', (event) => {
+  if (event.target && event.target.className === 'remove-book mb-05') {
+    removeBook(event.target);
+    setStorage();
+    clearBooksSection();
+    displayBooksSection(BOOKS_DATA);
+    updateSelections();
+  }
+});
+
 form.addEventListener('submit', (event) => {
   BOOKS_DATA.push(addBook());
+  setStorage();
   clearBooksSection();
   displayBooksSection(BOOKS_DATA);
   updateSelections();
   event.preventDefault();
 });
-
-function assignRemoveEvents() {
-  const removeBtns = document.querySelectorAll('.remove-book');
-  for (let i = 0; i < removeBtns.length; i += 1) {
-    removeBtns[i].addEventListener('click', () => {
-      removeBook(i);
-      clearBooksSection();
-      displayBooksSection(BOOKS_DATA);
-      updateSelections();
-    });
-  }
-}
