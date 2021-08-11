fbc55bd6aff5328f7ab7b225cd26e0de1dea7e5e
nelsinofrancisco <nelsino.francisco@gmail.com>
2021-08-11 15:18:18 -0300

    Add Css for add book section and Fix Issue with remove function

--- modules/list_section.js
+++ modules/list_section.js
@@ -16,17 +16,17 @@ export default class ListSection {
     this.sperator = document.createElement('span');
     this.removeBtn = document.createElement('button');
 
-    this.bookCard.classList.add('book-card', 'd-flex', 'align-items-center');
+    this.bookCard.classList.add('book-card', 'd-flex', 'align-items-center', 'p-1');
     this.bookTitle.classList.add('book-card-text', 'mb-0');
-    this.bookAuthor.classList.add('book-card-text', 'mb-0');
+    this.bookAuthor.classList.add('book-card-text', 'mb-0', 'me-5');
     this.sperator.classList.add('mx-1');
-    this.removeBtn.classList.add('remove-book', 'mb-05', 'ms-auto');
-
-    this.bookTitle.textContent = bookObj.title;
+    this.removeBtn.classList.add('remove-book', 'ms-auto', 'bg-white', 'border', 'border-2', 'border-dark', 'px-1');
+    const title = `"${bookObj.title}"`;
+    this.bookTitle.textContent = title;
     this.bookAuthor.textContent = bookObj.author;
     this.sperator.textContent = 'by';
     this.removeBtn.textContent = 'Remove';
-    
+
     this.bookCard.appendChild(this.bookTitle);
     this.bookCard.appendChild(this.sperator);
     this.bookCard.appendChild(this.bookAuthor);
